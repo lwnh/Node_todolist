7242a157848a8b12b6b3058ee638a345aaee8aa1
yumi <>
2021-01-21 17:07:17 +0900

    TodoItem 컴포넌트 생성
    - done, remove 기능

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
+import TodoItem from './TodoItem';
 
 const TodoListBlock = styled.div`
   flex: 1;
@@ -9,7 +10,14 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
-    return <TodoListBlock>TodoList</TodoListBlock>
+    return (
+      <TodoListBlock>
+          <TodoItem text="Nodejs 공부하기" done={true} />
+          <TodoItem text="Github 연동하기" done={true} />
+          <TodoItem text="Context API 강의듣기" done={false} />
+          <TodoItem text="자료구조 정리하기" done={false} />
+      </TodoListBlock>
+    )
 }
 
 export default TodoList
